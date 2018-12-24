550549d1032ac423e6145982458cf626ee997ac8
EvyHacker <gaidarenko1241@gmail.com>
2018-12-24 12:48:14 -0500

    added new method. Homework#2 completed

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,26 +1,15 @@
 
 public class Point {
 
-	
+	static Distance d = new Distance(4, 6, 4, 6);
 
 	
-//	public static double x2 = 0.5;
-//	public static double y1 = 0.2;
-//	public static double y2 = 0.5;
 
-		
-		 static Distance d = new Distance(4, 6, 4, 6);
-		
-		
+	public static void main(String[] args) {
+//		Point p1 = new Point();
+//		Point p2 = new Point();
+		System.out.println("With this coordinates: " +" x1=" + d.x1 +" x2=" + d.x2+" y1=" + d.y1+" y2=" + d.y2 );
+		System.out.println("Distance between two points "  + "=" + d.distance());
 
-		public static double distance(Point p1, Point p2) {
-			return Math.sqrt((d.x2 - d.x1) * (d.x2 - d.x1) + (d.y2 - d.y1) * (d.y2 - d.y1));
-		}
-
-		public static void main(String[] args) {
-			Point p1 = new Point();
-			Point p2 = new Point();
-			System.out.println("Distance between two points + " + distance(p1, p2));
-
-		}
-		}
+	}
+}
