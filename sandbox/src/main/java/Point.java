ef72db44e12f94245986f08eec90206a82b2150d
EvyHacker <gaidarenko1241@gmail.com>
2018-12-24 12:23:12 -0500

    added new class distance

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,13 +1,20 @@
 
 public class Point {
 
-		public static double x1 = 0.6;
-		public static double x2 = 0.5;
-		public static double y1 = 0.2;
-		public static double y2 = 0.5;
+	
+
+	
+//	public static double x2 = 0.5;
+//	public static double y1 = 0.2;
+//	public static double y2 = 0.5;
+
+		
+		 static Distance d = new Distance(4, 6, 4, 6);
+		
+		
 
 		public static double distance(Point p1, Point p2) {
-			return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+			return Math.sqrt((d.x2 - d.x1) * (d.x2 - d.x1) + (d.y2 - d.y1) * (d.y2 - d.y1));
 		}
 
 		public static void main(String[] args) {
@@ -16,4 +23,4 @@ public class Point {
 			System.out.println("Distance between two points + " + distance(p1, p2));
 
 		}
-	}
+		}
