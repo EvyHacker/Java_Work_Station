9065e0ce77afc021d572dac605bbd7360abc3ede
EvyHacker <gaidarenko1241@gmail.com>
2018-12-26 22:06:20 -0500

    home work #1 paragraph 1-3

--- sandbox/src/main/java/usa/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/usa/stqa/pft/sandbox/Point.java
@@ -1,12 +1,28 @@
 package usa.stqa.pft.sandbox;
 
+
 public class Point {
 
-	static Distance d = new Distance(0.6, 0.5, 0.2, 0.5);
-
+	public double x;
+	public double y;
+	
+	public Point(double x, double y) { 
+	this.x = x;
+	this.y = y;
+	}
+ public static double distance(Point p1, Point p2) {
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p1.y - p2.y) * (p1.y - p2.y));
+	}
+ 
+ public String toString() {
+     return "(" + x + ", " + y + ")";
+ }
+ 
 	public static void main(String[] args) {
-		System.out.println("With this coordinates: " + " x1=" + d.x1 + " x2=" + d.x2 + " y1=" + d.y1 + " y2=" + d.y2);
-		System.out.println("Distance between two points " + "=" + d.distance());
+		Point p1 = new Point(4, 4);
+		Point p2 = new Point(6,6 );
+		System.out.println("Distance between two points = " + distance(p1, p2));
 
 	}
+
 }
