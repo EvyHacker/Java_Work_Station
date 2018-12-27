161d5d6dd2a2b80c6863c7bdf308ebc283f8f141
EvyHacker <gaidarenko1241@gmail.com>
2018-12-26 23:30:52 -0500

    homework #2

--- sandbox/src/main/java/usa/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/usa/stqa/pft/sandbox/Point.java
@@ -1,28 +1,31 @@
 package usa.stqa.pft.sandbox;
 
+public class Point { 
+    private double x;   
+    private double y;   
+    public static double dx;
+    public static double dy;
+   
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-public class Point {
+    public double distance(Point that) {
+        double dx = this.x - that.x;
+        double dy = this.y - that.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
 
-	public double x;
-	public double y;
-	
-	public Point(double x, double y) { 
-	this.x = x;
-	this.y = y;
-	}
- public static double distance(Point p1, Point p2) {
-		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p1.y - p2.y) * (p1.y - p2.y));
-	}
- 
- public String toString() {
-     return "(" + x + ", " + y + ")";
- }
- 
-	public static void main(String[] args) {
-		Point p1 = new Point(4, 4);
-		Point p2 = new Point(6,6 );
-		System.out.println("Distance between two points = " + distance(p1, p2));
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 
-	}
-
+    public static void main(String[] args) {
+        Point p1 = new Point(0.6, 0.2);
+        System.out.println("p1  = " + p1);
+        Point p2 = new Point(0.5, 0.5);
+        System.out.println("p2  = " + p2);
+        System.out.println("dist(p, q) = " + p1.distance(p2));
+    }
 }
