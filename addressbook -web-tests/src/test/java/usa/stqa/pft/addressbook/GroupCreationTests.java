76103101db580188288efa4fb69b28d0b619967c
EvyHacker <gaidarenko1241@gmail.com>
2019-01-04 21:34:28 -0500

    Created Delete Group Tests

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,79 +1,14 @@
 package usa.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class GroupCreationTests {
-  private WebDriver wd;
-  private StringBuffer verificationErrors = new StringBuffer();
-
-  @BeforeMethod
-
-  public void setUp() throws Exception {
-	System.setProperty("webdriver.chrome.driver", "/Users/ievgeniiagaidarenko/JAVA/chromedriver 2");
-    wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/edit.php");
-    login("admin", "secret");
-  }
-
-private void login(String username, String password) {
-	wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-}
-
-  @Test
-  public void testGroupCreation() {
-    gotoGroupPage();
-    initGroupCreation();
-    fillGroupForm(new GroupData("test1", "test2", "test3"));
-    submitGroupCreation();
-    returnToGroupPage();
-  }
-
-private void returnToGroupPage() {
-	wd.findElement(By.linkText("Logout")).click();
-}
-
-private void submitGroupCreation() {
-	wd.findElement(By.name("submit")).click();
-}
-
-private void fillGroupForm(GroupData groupData) {
-	wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-}
-
-private void initGroupCreation() {
-	wd.findElement(By.name("new")).click();
-}
-
-private void gotoGroupPage() {
-	wd.findElement(By.linkText("groups")).click();
-}
-
-  @AfterMethod
-
-  public void tearDown() throws Exception {
-    wd.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      Assert.fail(verificationErrorString);
-    }
-  }
+public class GroupCreationTests extends TestBase {
+	@Test
+	public void testGroupCreation() {
+		gotoGroupPage();
+		initGroupCreation();
+		fillGroupForm(new GroupData("test1", "test2", "test3"));
+		submitGroupCreation();
+		returnToGroupPage();
+	}
 }
---
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package usa.stqa.pft.addressbook;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+
+public class GroupDeletionTests extends TestBase {
+
+	@Test
+	public void testGroupDeletion() {
+		gotoGroupPage();
+		selectGroup();
+		deleteSelectedGroups();
+		returnToGroupPage();
+	}
+
+}
