6319d2410dda69654741ff36b92b97f1a8451257
EvyHacker <gaidarenko1241@gmail.com>
2019-01-04 20:09:06 -0500

    little more clean up

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/ClassCreationClass.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/ClassCreationClass.java
@@ -5,34 +5,30 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ClassCreationClass {
 
-	private WebDriver driver;
-	private boolean acceptNextAlert = true;
+	private WebDriver wd;
 	private StringBuffer verificationErrors = new StringBuffer();
 
 	@BeforeClass(alwaysRun = true)
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "/Users/ievgeniiagaidarenko/JAVA/chromedriver 2");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		wd = new ChromeDriver();
+		wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		login("admin", "secret");
 	}
 
 	private void login(String username, String password) {
-		driver.get("http://localhost/addressbook/index.php");
-		driver.findElement(By.name("user")).clear();
-		driver.findElement(By.name("user")).sendKeys(username);
-		driver.findElement(By.name("pass")).clear();
-		driver.findElement(By.name("pass")).sendKeys(password);
-		driver.findElement(
+		wd.get("http://localhost/addressbook/index.php");
+		wd.findElement(By.name("user")).clear();
+		wd.findElement(By.name("user")).sendKeys(username);
+		wd.findElement(By.name("pass")).clear();
+		wd.findElement(By.name("pass")).sendKeys(password);
+		wd.findElement(
 				By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]"))
 				.click();
 	}
@@ -46,38 +42,38 @@ public class ClassCreationClass {
 	}
 
 	private void logout() {
-		driver.findElement(By.linkText("Logout")).click();
+		wd.findElement(By.linkText("Logout")).click();
 	}
 
 	private void submitNewContact() {
-		driver.findElement(
+		wd.findElement(
 				By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]"))
 				.click();
 	}
 
 	private void fillFormContact(GroupContactData parameterObject) {
-		driver.findElement(By.name("firstname")).click();
-		driver.findElement(By.name("firstname")).clear();
-		driver.findElement(By.name("firstname")).sendKeys(parameterObject.getFirstName());
-		driver.findElement(By.name("lastname")).click();
-		driver.findElement(By.name("lastname")).clear();
-		driver.findElement(By.name("lastname")).sendKeys(parameterObject.getLastName());
-		driver.findElement(By.name("mobile")).click();
-		driver.findElement(By.name("mobile")).clear();
-		driver.findElement(By.name("mobile")).sendKeys(parameterObject.getMobileNumber());
-		driver.findElement(By.name("email")).click();
-		driver.findElement(By.name("email")).clear();
-		driver.findElement(By.name("email")).sendKeys(parameterObject.getEmailAddress());
+		wd.findElement(By.name("firstname")).click();
+		wd.findElement(By.name("firstname")).clear();
+		wd.findElement(By.name("firstname")).sendKeys(parameterObject.getFirstName());
+		wd.findElement(By.name("lastname")).click();
+		wd.findElement(By.name("lastname")).clear();
+		wd.findElement(By.name("lastname")).sendKeys(parameterObject.getLastName());
+		wd.findElement(By.name("mobile")).click();
+		wd.findElement(By.name("mobile")).clear();
+		wd.findElement(By.name("mobile")).sendKeys(parameterObject.getMobileNumber());
+		wd.findElement(By.name("email")).click();
+		wd.findElement(By.name("email")).clear();
+		wd.findElement(By.name("email")).sendKeys(parameterObject.getEmailAddress());
 	}
 
 	private void addNewContact() {
-		driver.findElement(By.linkText("add new")).click();
+		wd.findElement(By.linkText("add new")).click();
 	}
 
 	@AfterClass(alwaysRun = true)
 	public void tearDown() throws Exception {
 		Thread.sleep(3000);
-		driver.quit();
+		wd.quit();
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,11 +1,8 @@
 package usa.stqa.pft.addressbook;
 
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -14,26 +11,25 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests {
-  private WebDriver driver;
-  private boolean acceptNextAlert = true;
+  private WebDriver wd;
   private StringBuffer verificationErrors = new StringBuffer();
 
   @BeforeMethod
 
   public void setUp() throws Exception {
 	System.setProperty("webdriver.chrome.driver", "/Users/ievgeniiagaidarenko/JAVA/chromedriver 2");
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/edit.php");
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/edit.php");
     login("admin", "secret");
   }
 
 private void login(String username, String password) {
-	driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+	wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
 }
 
   @Test
@@ -46,35 +42,35 @@ private void login(String username, String password) {
   }
 
 private void returnToGroupPage() {
-	driver.findElement(By.linkText("Logout")).click();
+	wd.findElement(By.linkText("Logout")).click();
 }
 
 private void submitGroupCreation() {
-	driver.findElement(By.name("submit")).click();
+	wd.findElement(By.name("submit")).click();
 }
 
 private void fillGroupForm(GroupData groupData) {
-	driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+	wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
 }
 
 private void initGroupCreation() {
-	driver.findElement(By.name("new")).click();
+	wd.findElement(By.name("new")).click();
 }
 
 private void gotoGroupPage() {
-	driver.findElement(By.linkText("groups")).click();
+	wd.findElement(By.linkText("groups")).click();
 }
 
   @AfterMethod
 
   public void tearDown() throws Exception {
-    driver.quit();
+    wd.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       Assert.fail(verificationErrorString);
