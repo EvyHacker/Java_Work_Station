4dfe5639643d55782fc4ac41a31fa83bb7b7cc34
EvyHacker <gaidarenko1241@gmail.com>
2019-01-30 16:11:53 -0500

    HW #7 Chrome and Firefox

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/TestBase.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/TestBase.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeMethod;
 import usa.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.SAFARI);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod
     public void setUp() throws Exception {
