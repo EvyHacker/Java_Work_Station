ba5a37323ee9f01c59525562bb4ebb83650fed49
EvyHacker <gaidarenko1241@gmail.com>
2019-01-30 15:47:14 -0500

    Safari HW #7

---
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupCreationSafariTest.java
@@ -0,0 +1,91 @@
+package usa.stqa.pft.addressbook.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+
+
+public class GroupCreationSafariTest {
+  private WebDriver driver;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new SafariDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+
+  @Test
+  public void testGroupCreationSafari() throws Exception {
+    driver.get("http://localhost/addressbook/group.php");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//*[@id=\"LoginForm\"]/input[3]")).click();
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//*[@id=\"nav\"]/ul/li[3]/a")).click(); //go to group page
+    driver.findElement(By.name("new")).click(); // add new group button
+    Thread.sleep(3000);
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys("TestSafari");
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys("Safari1");
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys("Safari2");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("home")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
