5a9083a77ac023d19be79a9b0926201f709321f3
EvyHacker <gaidarenko1241@gmail.com>
2019-02-17 17:00:26 -0500

    List Groups comparison

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupModificationTest.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupModificationTest.java
@@ -4,24 +4,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import usa.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTest extends TestBase {
 
 	@Test
 	public void testGroupModification() {
 		app.getNavigationHelper().gotoGroupPage();
-		int before = app.getGroupHelper().getGroupCount();
 		if (!app.getGroupHelper().isThereAGroup()) {
 			app.getGroupHelper()
 					.createGroup(new GroupData("test1", null, null));
 		}
-		app.getGroupHelper().selectGroup();
+		List<GroupData> before = app.getGroupHelper().getGroupList();
+		//int before = app.getGroupHelper().getGroupCount();
+		app.getGroupHelper().selectGroup(before.size()-1);
 		app.getGroupHelper().initGroupModification();
 		app.getGroupHelper()
 				.fillGroupForm(new GroupData("test4", "test5", "test6"));
 		app.getGroupHelper().submitGroupModification();
 		app.getGroupHelper().returnToGroupPage();
-		int after = app.getGroupHelper().getGroupCount();
-		Assert.assertEquals(after, before);
+		List<GroupData> after = app.getGroupHelper().getGroupList();
+		Assert.assertEquals(after.size(), before.size());
+//		int after = app.getGroupHelper().getGroupCount();
+//		Assert.assertEquals(after, before);
 
 	}
 }
