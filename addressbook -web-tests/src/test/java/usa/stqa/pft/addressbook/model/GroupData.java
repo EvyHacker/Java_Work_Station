53b0da4db505ac4bf7000a3bcedb22e1ee5178e1
EvyHacker <gaidarenko1241@gmail.com>
2019-02-17 23:02:06 -0500

    GroupCreation comparison

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/model/GroupData.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/model/GroupData.java
@@ -15,8 +15,9 @@ public class GroupData {
 	public int getId() {
 		return id;
 	}
+
 	public GroupData(String name, String header, String footer) {
-		this.id = 0;
+		this.id = Integer.MAX_VALUE;
 		this.name = name;
 		this.header = header;
 		this.footer = footer;
@@ -42,20 +43,6 @@ public class GroupData {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		GroupData groupData = (GroupData) o;
-		return id == groupData.id &&
-				Objects.equals(name, groupData.name);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
-	}
-
-	@Override
 	public String toString() {
 		return "GroupData{" +
 				"id='" + id + '\'' +
@@ -63,4 +50,16 @@ public class GroupData {
 				'}';
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GroupData groupData = (GroupData) o;
+		return Objects.equals(name, groupData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
 }
--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupCreationTest.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupCreationTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import usa.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -16,23 +16,19 @@ public class GroupCreationTest extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
         //int before = app.getGroupHelper().getGroupCount();
-        GroupData group= new GroupData("test1", null, null);
+        GroupData group= new GroupData("test2", null, null);
         app.getGroupHelper().createGroup(group);
         // int after = app.getGroupHelper().getGroupCount();
         //Assert.assertEquals(after, before + 1);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);
 
+        before.add(group);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
-            before.add(group);
-        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
     }
 }
