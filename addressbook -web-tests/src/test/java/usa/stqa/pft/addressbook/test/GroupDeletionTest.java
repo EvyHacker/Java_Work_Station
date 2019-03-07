ea444d45a94356323d66c330735fd9ef0eedf7a0
EvyHacker <gaidarenko1241@gmail.com>
2019-03-06 20:47:59 -0500

    GroupDeletion DB

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupDeletionTest.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupDeletionTest.java
@@ -6,25 +6,25 @@ import usa.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
-import static org.testng.Assert.assertEquals;
+
 
 public class GroupDeletionTest extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().groupPage();
         if (app.group().all().size() == 0) {
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
         }
     }
 
     @Test
     public void testGroupDeletion() {
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
         app.group().delete(deletedGroup);
         assertThat(app.group().count(), equalTo(before.size()-1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.withoutAdded(deletedGroup)));
         }
 
