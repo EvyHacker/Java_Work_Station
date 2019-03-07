0c4efaf85a969d2725517d5d7e55e17cd3082099
EvyHacker <gaidarenko1241@gmail.com>
2019-03-06 20:45:49 -0500

    GroupCreation DB

--- addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupCreationTest.java
+++ addressbook -web-tests/src/test/java/usa/stqa/pft/addressbook/test/GroupCreationTest.java
@@ -1,14 +1,17 @@
 package usa.stqa.pft.addressbook.test;
-
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import usa.stqa.pft.addressbook.model.GroupData;
 import usa.stqa.pft.addressbook.model.Groups;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,13 +24,26 @@ public class GroupCreationTest extends TestBase {
 
 
     @DataProvider
+    public Iterator<Object[]> validGroupsFromCsv() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] split = line.split(";");
+                list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+                line = reader.readLine();
+            }
+            return list.iterator();
+        }
+    }
+
+    @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
             String xml = "";
             String line = reader.readLine();
             while (line != null) {
                 xml += line;
-
                 line = reader.readLine();
             }
             XStream xstream = new XStream();
@@ -39,42 +55,39 @@ public class GroupCreationTest extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromJson() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"))){
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
             String json = "";
             String line = reader.readLine();
-            while (line != null){
+            while (line != null) {
                 json += line;
                 line = reader.readLine();
             }
-            Gson gson =new Gson();
-            List <GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());//<List<GroupData>.class
-            return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+            Gson gson = new Gson();
+            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+            }.getType()); // List<GroupData>.class
+            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
     }
 
     @Test(dataProvider = "validGroupsFromJson")
-    //public void testGroupCreation(String name, String header, String footer) {
-        public void testGroupCreation(GroupData group) {
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
-//        String[] names = new String[]{"test1", "test2", "test3"};
-//        for (String name : names) {
-            Groups before = app.group().all();
-            app.group().create(group);
-            assertThat(app.group().count(), equalTo(before.size() + 1));
-            Groups after = app.group().all();
-            assertThat(after, equalTo
-                    (before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
-        }
+        Groups before = app.db().groups();
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));//Проверка на основе хеширования
+        Groups after = app.db().groups();
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) ->g.getId()).max().getAsInt()))));
+    }
 
     @Test(enabled = false)
-    public void testBadGroupCreation() {
+    public void testBadGroupCreation() throws Exception {
+        Groups before = app.db().groups();
+        GroupData group = new GroupData().withName("test2'");
         app.goTo().groupPage();
-        Groups before = app.group().all();
-        GroupData group= new GroupData().withName("test'");
         app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size()));
-        Groups after = app.group().all();
+        assertThat(app.group().count(), equalTo(before.size()));//Проверка на основе хеширования
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before));
     }
 }
